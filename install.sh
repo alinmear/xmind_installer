95a871f7a8074e90a147c7988aafb2288578eb72
alinmear <paul.steinlechner@gmx.at>
2016-09-17 12:45:31 +0200

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+_version="7.5-update1"
+_wdir=`mktemp -d` && cd $dir
+
+echo "Starting Xmind Installation"
+
+echo "  * Downloading:"
+wget -U 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' http://www.xmind.net/xmind/downloads/xmind-${_version}-linux_amd64.deb || exit 1
+
+echo "  * Extracting DEB:"
+ar -x xmind-${_version}-linux_amd64.deb  || echo "==> Warning: failed to extract"
+echo "  * Exctracting data.tar.gz:"
+tar xf data.tar.gz  || echo "==> Warning: failed to extract"
+
+echo "  * Moving xmind to /opt"
+[ -d /opt/xmind ] && rm -rf /opt/xmind && echo "/opt/xmind already existing. Removed it .."
+mv usr/lib/xmind /opt/ || echo "==> Warning: failed to move xmind to /opt"
+
+echo "  * Moving XMind.ini"
+mv etc/XMind.ini /opt/xmind/ ||  echo "==> Warning: failed to move XMind.ini to /opt/xmind"
+echo "  * Changing XMind lib path to /opt/xmind/XMind"
+sed -i -e 's|^XMIND=.*|XMIND=/opt/xmind/XMind|g' usr/bin/XMind || echo "==> Warning: failed to substitue"
+
+echo "  * Moving Xmind to /usr/bin/XMind"
+mv usr/bin/XMind /usr/bin/ || echo "==> Warning: failed to move"
+
+echo "  * Copying usr/share/* to /usr/share"
+cp -Rv usr/share/* /usr/share/ || echo "==> Warning: failed to copy"
+
+echo "  * Leaving working dir"
+cd ..
+
+echo "  * Removing working dir"
+rm -Rv "${_wdir}" || echo "==> Warning: failed to remove working dir"
+
+exit 0
